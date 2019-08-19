51445ef45544abee74c099e019cc021609d3c913
anar <arnold.angerer@skidata.com>
2019-08-19 14:16:08 +0200

    added zooming, panning, default view, copy, paste

--- src/app/fabric-view/fabric-view.component.ts
+++ src/app/fabric-view/fabric-view.component.ts
@@ -12,9 +12,14 @@ export class FabricViewComponent implements OnInit, AfterViewInit {
 
   canvas: any;
   isDragging: boolean;
-  selection: boolean;
+
   lastPosX: number;
   lastPosY: number;
+  zoom: number;
+  flip: boolean;
+  i = 0;
+  clipboard: any;
+  
 
   constructor() { }
 
@@ -23,25 +28,65 @@ export class FabricViewComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
 
     this.canvas = new fabric.Canvas('canvas');
-    console.log(mapJson);
     this.canvas.loadFromJSON(mapJson);
-    
+
+    // zooming
+    this.canvas.on('mouse:wheel', opt => {
+      const delta = opt.e.deltaY;
+      let zoom = this.canvas.getZoom() + delta / 800;
+      if (zoom > 20) {
+        zoom = 20;
+      } else if (zoom < 0.01) {
+        zoom = 0.01;
+      }
+      this.canvas.setZoom(zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+    });
+
+    // panning
+    this.canvas.on('mouse:down', opt => {
+      const evt = opt.e;
+      if (evt.altKey === true) {
+        this.isDragging = true;
+        this.canvas.selection = false;
+        this.lastPosX = evt.clientX;
+        this.lastPosY = evt.clientY;
+      }
+    });
+
+    this.canvas.on('mouse:move', opt => {
+      if (this.isDragging) {
+        const e = opt.e;
+        this.canvas.viewportTransform[4] += e.clientX - this.lastPosX;
+        this.canvas.viewportTransform[5] += e.clientY - this.lastPosY;
+        this.canvas.requestRenderAll();
+        this.lastPosX = e.clientX;
+        this.lastPosY = e.clientY;
+      }
+    });
+
+    this.canvas.on('mouse:up', opt => {
+      this.isDragging = false;
+      this.canvas.selection = true;
+      this.canvas.getObjects().forEach(o => {
+        o.setCoords();
+      });
+    });
+    console.log(this.canvas.getZoom());
+
   }
   add_rect() {
-    let rect = new fabric.Rect({
+    const rect = new fabric.Rect({
       top: 0,
       left: 0,
       width: 60,
       height: 70,
       fill: 'rgba(255,0,0,0.3)',
       selectable: true,
-      
     });
     this.canvas.add(rect);
-    
   }
-  flip: boolean;
-  i = 0;
   get nextEntry() {
     return ++this.i;
   }
@@ -54,45 +99,36 @@ export class FabricViewComponent implements OnInit, AfterViewInit {
       img.scale(0.3);
       img.on('selected', _ => {
         console.log(this.canvas.getActiveObject().id);
-        
-        
-      })
+      });
     });
   }
   add_text() {
-    const text = new fabric.IText("Entry 1", { left :100, top: 100});
+    const text = new fabric.IText('Entry 1', { left: 100, top: 100 });
     this.canvas.add(text);
   }
   add_apm() {
     fabric.Image.fromURL('/assets/apm.jpg', img => {
-      this.canvas.add(img);      
+      this.canvas.add(img);
       img.on('selected', _ => {
-        console.log(this.canvas.getActiveObject().id);        
+        console.log(this.canvas.getActiveObject().id);
       })
     });
   }
   add_background() {
-    fabric.Image.fromURL('/assets/airportmap.png', img => {            
-      // this.canvas.setBackgroundImage(img, this.canvas.renderAll.bind(this.canvas),{
-      //   originX: 'left',
-      //   originY: 'top'
-      // });
-      // this.canvas.backgroundImage.width = this.canvas.getWidth();
-      // this.canvas.backgroundImage.height = this.canvas.getHeight();
-        this.canvas.setBackgroundImage(img, this.canvas.renderAll.bind(this.canvas), {
-          scaleX: this.canvas.getWidth() / img.width,
-          scaleY: this.canvas.getHeight() / img.height
-        });      
+    fabric.Image.fromURL('/assets/airportmap.png', img => {
+      this.canvas.setBackgroundImage(img, this.canvas.renderAll.bind(this.canvas), {
+        originX: 'left',
+        originY: 'top',
+        scaleX: this.canvas.getWidth() / img.width,
+        scaleY: this.canvas.getHeight() / img.height
+      });
     });
-    
-
   }
   delete_object() {
     const obj = this.canvas.getActiveObject();
-    if(obj) {
+    if (obj) {
       this.canvas.remove(obj);
     }
-    
   }
   delete_background() {
     console.log('de')
@@ -101,17 +137,51 @@ export class FabricViewComponent implements OnInit, AfterViewInit {
   get_object_names() {
     this.canvas.getObjects().forEach(o => {
       console.log(o.id);
-    })
+    });
   }
   set_object_image() {
-    let entry1 = this.canvas.getObjects().find(x => x.id === "Entry: 1");
+    const entry1 = this.canvas.getObjects().find(x => x.id === "Entry: 1");
     console.log(entry1);
     entry1.setSrc('/assets/barrier1.png', this.canvas.renderAll.bind(this.canvas));
   }
   serialize() {
+    console.log('serializing');
     console.log(JSON.stringify(this.canvas));
   }
+  to_default_view() {    
+    this.canvas.setViewportTransform([1,0,0,1,0,0]);;
+  }
+  copy() {
+    this.canvas.getActiveObject().clone(clone => {
+      this.clipboard = clone;
+    });
+  }
+  paste() {
+    this.clipboard.clone(clone => {
+      this.canvas.discardActiveObject();
+      clone.set({
+        left: clone.left + 10,
+        top: clone.top + 10,
+        evented: true
+      });
+      if(clone.type === 'activeSelection') {
+        clone.canvas = this.canvas;
+        clone.forEachObject(obj => {
+          this.canvas.add(obj);
+        });
+        clone.setCoords();
+      } else {
+        this.canvas.add(clone);
+      }
+      this.clipboard.top += 10;
+      this.clipboard.left += 10;
+      this.canvas.setActiveObject(clone);
+      this.canvas.requestRenderAll();
+    });
 
+  }
   
+
+
 
 }
